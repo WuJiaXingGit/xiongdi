30a65bfb4679af054e37d2b3ea786a8e83fd1723
WuJiaXingGit <287475833@qq.com>
2019-06-27 16:12:24 +0800

    Initial commit

---
+++ xiongdi-houduan/xiongdi-generator/src/main/java/io/xiongdi/GeneratorApplication.java
@@ -0,0 +1,16 @@
+package io.xiongdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wujiaxing
+ * @date 2019-06-27
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+}
