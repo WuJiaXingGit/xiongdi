e6b69fce76bd4ae47c9932e0f7cb82d8c1600e90
WuJiaXingGit <287475833@qq.com>
2019-09-25 01:25:19 +0800

    新增分组

---
+++ xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/AddGroup.java
@@ -0,0 +1,9 @@
+package io.xiongdi.common.validator.group;
+
+/**
+ * 添加组
+ * @author wujiaxing
+ * @date 2019-09-25
+ */
+public interface AddGroup {
+}
---
+++ xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/UpdateGroup.java
@@ -0,0 +1,9 @@
+package io.xiongdi.common.validator.group;
+
+/**
+ * 更新组
+ * @author wujiaxing
+ * @date 2019-09-25
+ */
+public interface UpdateGroup {
+}
