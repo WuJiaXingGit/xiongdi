c5d9ca4411b8c8fb3b3a5902db7278baa0203b71
WuJiaXingGit <287475833@qq.com>
2019-09-25 00:25:46 +0800

    显示提交

--- xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/AddGroup.java
+++ xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/AddGroup.java
@@ -2,7 +2,7 @@ package io.xiongdi.common.validator.group;
 
 /**
  * 添加数据 group
- * @author wujiaxing
+ * @author wujiaxinga
  * @date 2019-07-14
  */
 public interface AddGroup {
--- xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/UpdateGroup.java
+++ xiongdi-houduan/xiongdi-common/src/main/java/io/xiongdi/common/validator/group/UpdateGroup.java
@@ -2,7 +2,7 @@ package io.xiongdi.common.validator.group;
 
 /**
  * 更新数据 group
- * @author wujiaxing
+ * @author wujiaxinga
  * @date 2019-07-14
  */
 public interface UpdateGroup {
